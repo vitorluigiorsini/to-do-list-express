e830191de74055e9e76a5d59b9d2c43055c60be8
Vitor Orsini <vitorl85@gmail.com>
2022-08-03 17:37:36 -0300

    Rotas PUT e DELETE

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,19 +2,34 @@ const express = require("express");
 
 const router = express.Router();
 
+// rota GET
 router.get("/", (req, res) => {
   console.log("Olá");
   res.send();
 });
 
+// rota POST
 router.post("/", (req, res) => {
   console.log(req.body);
   res.status(200).send(req.body);
 });
 
+// adicionando parâmetro na rota
 router.get("/:id", (req, res) => {
-  console.log(req.params.id);
+  console.log(req.body);
   res.send(`ID: ${req.params.id}`);
+});
+
+// rota PUT
+router.put("/:id", (req, res) => {
+  console.log(req.body);
+  res.send(`PUT ID: ${req.params.id}`);
+});
+
+// rota DELETE
+router.delete("/:id", (req, res) => {
+  console.log(req.body);
+  res.send(`DELETE ID: ${req.params.id}`);
 });
 
 module.exports = router;
