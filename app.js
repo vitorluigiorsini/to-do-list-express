68f7c8f754b84bb7492e67ce005a313616268f00
Vitor Orsini <vitorl85@gmail.com>
2022-08-03 16:31:15 -0300

    Primeira rota com Express

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const express = require("express");
+const express = require("express"); // importando o express
+
+const app = express();
+
+// método GET
+app.get("/", (req, res) => {
+  res.send("<h1>Minha lista de tarefas :)</h1>");
+});
+
+// ouvindo porta 3000
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado");
+});
