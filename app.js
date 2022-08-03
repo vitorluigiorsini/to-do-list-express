3f85fb0535934d1a54d71a94544c168b9f2191a6
Vitor Orsini <vitorl85@gmail.com>
2022-08-03 16:35:28 -0300

    Responsa em JSON

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
   res.send("<h1>Minha lista de tarefas :)</h1>");
 });
 
+// resposta em JSON
+app.get("/json", (req, res) => {
+  res.json({ title: "Tarefa X", done: true });
+});
+
 // ouvindo porta 3000
 app.listen(3000, () => {
   console.log("Servidor foi iniciado");
