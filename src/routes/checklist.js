272393b3b21f2690ef42c66aa672536460e7574c
Vitor Orsini <vitorl85@gmail.com>
2022-08-03 17:32:42 -0300

    Parâmetros nas rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -12,4 +12,9 @@ router.post("/", (req, res) => {
   res.status(200).send(req.body);
 });
 
+router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send(`ID: ${req.params.id}`);
+});
+
 module.exports = router;
