712ed6f42d9182282a07d010b53fd25f53dbfd56
Vitor Orsini <vitorl85@gmail.com>
2022-08-04 19:26:49 -0300

    Insere dados através de rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -8,9 +8,11 @@ const Checklist = require("../models/checklist"); // importando o model checklis
 router.get("/", async (req, res) => {
   try {
     let checklists = await Checklist.find({});
-    res.status(200).json(checklists);
+    res.status(200).render("checklists/index", { checklists: checklists });
   } catch (error) {
-    res.status(500).json(error);
+    res
+      .status(500)
+      .render("pages/error", { error: "Erro ao exibir as listas" });
   }
 });
 
@@ -30,9 +32,11 @@ router.post("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     let checklist = await Checklist.findById(req.params.id);
-    res.status(200).json(checklist);
+    res.status(200).render("checklists/show", { checklist: checklist });
   } catch (error) {
-    res.status(422).json(error);
+    res
+      .status(500)
+      .render("pages/error", { error: "Erro ao exibir as listas de tarefas" });
   }
   // console.log(req.body);
   // res.send(`ID: ${req.params.id}`);
